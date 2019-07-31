df5bcee3cd7e1feb8f64f290a6e462f56b84c582
Ali Halataee <halataa@gmail.com>
2019-07-31 16:27:13 +0430

    y

--- generateData.py
+++ generateData.py
@@ -7,17 +7,16 @@ from PIL import ImageFont
 from PIL import Image
 from PIL import ImageDraw
 import numpy as np
-
-#%%
 import wikipedia
 import re
+
 wikipedia.set_lang("fa")
 #%%
 summaryNumber=1
 for i in range (0,summaryNumber):
     summary=wikipedia.summary(wikipedia.random())
     pureSummary=re.sub('[^\s.،!؟?):/(٪|ء-ی|۰-۹|0-9]','',summary)  #eliminate english characters
-puresummaryList=pureSummary.split(' ')
+pure_summary_list=pureSummary.split(' ')
 
 #%%
 # Creating image file
